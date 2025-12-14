494e6fb2d7972524ee8417c9aa445512d5f64eab
anoushayzeaghum <anoushayzeaghum2001@gmail.com>
2025-12-15 02:02:52 +0500

    Initial commit: backend and frontend setup

---
+++ Backend/server/settings/local.py
@@ -0,0 +1,25 @@
+from .base import *
+
+DEBUG = True
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+
+# CORS – allow your React dev server
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',  
+        'NAME': get_secret("DB_NAME", "Ruda"),
+        'USER': get_secret("DB_USER", "postgres"),
+        'PASSWORD': get_secret("DB_PASSWORD", "postgres"),
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+GDAL_LIBRARY_PATH = secrets["GDAL_LIBRARY_PATH"]
+GEOS_LIBRARY_PATH = secrets["GEOS_LIBRARY_PATH"]
